b84f1ec3e66863948d7ac4055fac51a2b4cb55a1
Paul Smith <paul@blinkylights.org>
2016-06-01 17:42:30 -0400

    Use the right methods

--- app/bot/index.js
+++ app/bot/index.js
@@ -5,7 +5,7 @@ var config = require('../conf'),
     MaesterBot = require('./maesterbot');
 
 var controller = botkit.slackbot({
-  debug: config.get('bot:debug')
+  debug: true
 });
 
 var bot = controller.spawn({
@@ -16,7 +16,7 @@ var maesterBot = new MaesterBot();
 
 controller.hears(['\bhello\b','\bhi\b'], 'direct_message,direct_mention,mention', maesterBot.hello);
 controller.hears(['\bhelp\b'], 'direct_message,direct_mention,mention', maesterBot.help);
-controller.hears(['^book\b\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.help);
-controller.hears(['^character\b\s*(.*)\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.help);
-controller.hears(['^house\b\s*(.*)\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.help);
+controller.hears(['^book\b\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.book);
+controller.hears(['^character\b\s*(.*)\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.character);
+controller.hears(['^house\b\s*(.*)\s*(.*)'], 'direct_message,direct_mention,mention', maesterBot.house);
 // controller.on('direct_message', maesterBot.question);
